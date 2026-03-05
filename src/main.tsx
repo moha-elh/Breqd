4d90f75a269741c4a78dc86a6b5da2ac1b07a878
moha <mohsineelhaouary@gmail.com>
2026-03-05 12:42:40 +0000

    fixed the build issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import Game from './Game.tsx'
 
