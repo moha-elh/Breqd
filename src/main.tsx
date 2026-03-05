b3c18c57467577294432d93307144744dfaf6f80
moha <mohsineelhaouary@gmail.com>
2026-03-05 12:28:13 +0000

    tracking analytics with vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import Game from './Game.tsx'
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Game />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
